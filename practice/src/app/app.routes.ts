a3da996a6010326ef0d3a14222a0160b1696984c
sabakublashvili1122 <sabakublashvili@ibsu.edu.ge>
2026-02-02 16:22:54 +0400

    task 3.1 complete

--- practice/src/app/app.routes.ts
+++ practice/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NoteList } from './note-list/note-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: "my-notes",
+        pathMatch: "full"
+    },
+    {
+        path: "my-notes",
+        component: NoteList
+    }
+];
