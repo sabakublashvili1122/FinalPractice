3cc8c3af26325421f6574c7f67505eb789d830b0
sabakublashvili1122 <sabakublashvili@ibsu.edu.ge>
2026-02-02 17:28:02 +0400

    task 4.2 complete

--- practice/src/app/app.routes.ts
+++ practice/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { NoteList } from './note-list/note-list';
+import { EditNote } from './edit-note/edit-note';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: "my-notes",
-        pathMatch: "full"
+        redirectTo: 'my-notes',
+        pathMatch: 'full'
     },
     {
-        path: "my-notes",
+        path: 'my-notes',
         component: NoteList
+    },
+    {
+        path: 'my-notes/:id/edit',
+        component: EditNote
     }
 ];
--- practice/src/app/edit-note/edit-note.ts
+++ practice/src/app/edit-note/edit-note.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-note',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-note.html',
   styleUrl: './edit-note.css',
 })
 export class EditNote {
 
+  form = new FormGroup({
+    title: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required),
+    isImportant: new FormControl(false)
+  })
 }
