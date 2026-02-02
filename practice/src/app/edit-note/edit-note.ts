c7d3c2e783041bb84947a8a9249e0fb3b6152a07
sabakublashvili1122 <sabakublashvili@ibsu.edu.ge>
2026-02-02 17:50:40 +0400

    task 4 complete

--- practice/src/app/edit-note/edit-note.ts
+++ practice/src/app/edit-note/edit-note.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NoteService } from '../note-service';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-edit-note',
@@ -7,11 +10,54 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './edit-note.html',
   styleUrl: './edit-note.css',
 })
-export class EditNote {
+export class EditNote implements OnInit{
+
+  router = inject(Router);
+  activeRoute = inject(ActivatedRoute);
+  noteService = inject(NoteService);
+
+  originalNote?: Note = undefined;
 
   form = new FormGroup({
     title: new FormControl('', Validators.required),
     content: new FormControl('', Validators.required),
     isImportant: new FormControl(false)
   })
+
+  onCancel(){
+    this.router.navigate(['my-notes']);
+  }
+
+  onSave(){
+    let value = this.form.value;
+
+    let updatedNote = {
+      id: this.originalNote?.id!,
+      title: value.title!,
+      content: value.content!,
+      isImportant: value.isImportant!,
+      createDate: this.originalNote?.createDate!
+    }
+
+    this.noteService.editNote(updatedNote).subscribe(c => {
+      this.router.navigate(['my-notes']);
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.activeRoute.paramMap.subscribe(p => {
+      let id = p.get('id');
+
+      this.noteService.getNote(id!).subscribe(c => {
+        this.form.setValue({
+          title: c.title,
+          content: c.content,
+          isImportant: c.isImportant
+        });
+
+        this.originalNote = c;
+      });
+    });
+  }
 }
